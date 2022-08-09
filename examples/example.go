7abde8319085c597315d50521b9184b345dcd4b5
saeveritt <saeveritt@gmail.com>
2022-08-09 18:34:00 -0500

    Create examples directory

---
+++ examples/example.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/saeveritt/go-peercoin-rpc/config"
+	"github.com/saeveritt/go-peercoin-rpc/jsonrpc"
+)
+
+func main() {
+	// Load the configuration file.
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Printf("Encountered an Error: %v\n", err)
+		return
+	}
+	// Create a new jsonrpc client.
+	client := jsonrpc.NewRPCClient(config)
+	// Call the getblockchaininfo method.
+	resp, err := client.Call("getblockchaininfo", nil)
+	if err != nil {
+		log.Printf("Encountered an Error: %v\n", err)
+		return
+	}
+	// Print the response.
+	log.Printf("Response: %v\n", resp)
+
+}
